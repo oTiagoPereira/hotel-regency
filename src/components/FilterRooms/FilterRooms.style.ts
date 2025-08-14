d3f7c9f68d886e8a8a1e0d5bc6cfcc476ff4c33e
Tiago Pereira <tiagopereira14200@gmail.com>
2025-08-14 14:05:45 +0000

    feat: adiciona opção de selecionar datas no filtro, e algumas alterações no DayPicker

--- src/components/FilterRooms/FilterRooms.style.ts
+++ src/components/FilterRooms/FilterRooms.style.ts
@@ -1,20 +1,39 @@
 export const FilterRoomsStyles = {
   filterOpenButtonWrapper: "md:hidden p-4 text-end",
-  buttonPrimary: "bg-primary text-text-neutral px-3 py-1 rounded-md hover:bg-primary-hover cursor-pointer font-semibold",
-  fixedButton: "bg-primary text-text-neutral px-3 py-1 rounded-md hover:bg-primary-hover cursor-pointer font-semibold fixed min-w-[80px] text-center z-50",
   mobileFilterContainer: (open: boolean) =>
-    `fixed inset-0 bg-transparent bg-opacity-50 z-10 transform transition-transform duration-300 ease-out md:static md:bg-transparent md:z-auto text-start ${
+    `fixed inset-0 bg-black bg-opacity-50 z-40 transform transition-transform duration-300 ease-out md:static md:bg-transparent md:z-40 text-start ${
       open ? "translate-y-0" : "translate-y-full"
     } md:translate-y-0`,
-  asideContainer: "bg-white min-w-full h-full md:min-w-6/12 md:rounded-md shadow-md mx-auto py-5 px-5 text-text-color overflow-y-auto",
+  asideContainer:
+    "relative bg-white min-w-full h-full md:min-w-6/12 md:rounded-md shadow-md mx-auto py-5 px-5 text-text-color",
   headerMobile: "flex justify-start items-start mb-4 md:hidden relative",
-  headerDesktop: "hidden md:flex justify-between items-center mb-4",
-  form: "space-y-6 mt-12 md:mt-2",
+  headerDesktop:
+    "hidden md:flex flex-wrap justify-between items-center mb-4 gap-2",
+  headerTitle: "text-2xl font-semibold",
+
+  buttonPrimary:
+    "bg-primary text-text-neutral px-3 py-1 rounded-md hover:bg-primary-hover cursor-pointer font-semibold",
+  resetButtonMobile:
+    "bg-primary text-text-neutral px-3 py-1 rounded-md hover:bg-primary-hover cursor-pointer font-semibold fixed min-w-[80px] text-center z-50 top-4 left-4",
+  closeButtonMobile:
+    "bg-primary text-text-neutral px-3 py-1 rounded-md hover:bg-primary-hover cursor-pointer font-semibold fixed min-w-[80px] text-center z-50 top-4 right-4",
+
+  form: "space-y-6 mt-12 md:mt-2 h-[calc(100%-100px)] md:h-auto overflow-y-auto",
   label: "font-medium block mb-2",
+  
+  datePickerWrapper:
+    "flex flex-wrap justify-between items-center gap-4",
+  dateDisplayBox:
+    "border border-gray-300 rounded-md px-3 py-2 w-auto text-center cursor-pointer flex-grow",
+
   checkboxContainer: "flex flex-col gap-1",
+  checkboxLabel: "flex items-center gap-2 cursor-pointer",
+  checkboxInput: "accent-primary cursor-pointer",
+
   priceRangeLabel: "font-medium block mb-4",
   priceRangeValues: "flex justify-between mt-3 h-auto",
   priceBox: "flex flex-col gap-1",
   priceLabelText: "text-sm text-gray-500",
-  priceValueText: "border border-gray-300 rounded-md px-3 py-2 text-sm font-medium",
+  priceValueText:
+    "border border-gray-300 rounded-md px-3 py-2 text-sm font-medium",
 };
--- src/components/RoomsSection/RoomsSection.style.ts
+++ src/components/RoomsSection/RoomsSection.style.ts
@@ -1,6 +1,6 @@
 export const RoomsSectionStyles = {
     section:
-      "w-full max-w-7xl mx-auto my-16 flex flex-col px-4 sm:px-6 lg:px-8 text-text-color text-center md:text-start font-body",
+      "w-full max-w-7xl mx-auto my-16 flex flex-col px-4 sm:px-6 lg:px-8 text-text-color text-center md:text-start font-body relative",
   
     heading: "text-3xl font-bold mb-2 font-heading",
   
@@ -8,7 +8,7 @@ export const RoomsSectionStyles = {
   
     mainWrapper: "flex flex-col md:flex-row gap-4 justify-between w-full",
   
-    filterWrapper: "w-full md:w-1/3 md:max-w-xs",
+    filterWrapper: "w-full md:w-1/3 md:max-w-xs relative z-20",
   
     gridWrapper:
       "grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full md:min-w-8/12 mx-auto lg:mx-0 lg:items-start justify-items-center",
