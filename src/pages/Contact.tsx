f90dfd48c3a2561b2457b7ab38f2889f2d01c5f1
Tiago Pereira <tiagopereira14200@gmail.com>
2025-08-31 16:52:24 -0300

    feat: adiciona página de contato com formulário e informações de contato

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import { Contact } from "../components";
+import { MainLayout } from "../layouts/mainLayout";
+
+function ContactPage() {
+    return (
+        <MainLayout>
+            <Contact/>
+        </MainLayout>
+    );
+}
+
+export default ContactPage;
